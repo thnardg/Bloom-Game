bd81db02476702e8afa1cf251f56175605e2a25b
Thayna <thna.rdg@gmail.com>
2023-07-14 14:29:09 -0300

    Setup do fluxo de telas

---
+++ mini_challenge_1/SliderLevel/CircleGlowScene.swift
@@ -0,0 +1,49 @@
+//
+//  CircleGlowScene.swift
+//  spritekitanimation
+//
+//  Created by Thayna Rodrigues on 13/07/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+// Cria um efeito de brilho para ser aplicado em qualquer SKSpriteNode. Mudar para a pasta utils?
+extension SKSpriteNode {
+    func addGlow(radius: Float = 30) {
+        let effectNode = SKEffectNode()
+        effectNode.shouldRasterize = true
+        addChild(effectNode)
+        let effect = SKSpriteNode(texture: texture)
+        effect.color = .cyan // Mudar para .self.color
+        effect.size = self.size
+        effect.colorBlendFactor = 1
+        effectNode.addChild(effect)
+        effectNode.filter = CIFilter(name: "CIGaussianBlur", parameters: ["inputRadius":radius])
+    }
+}
+
+class CircleGlowScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let circle = SKSpriteNode(imageNamed: "O") // Node da forma inicial
+        circle.size = CGSize(width: 100, height: 100)
+        circle.position = CGPoint(x: frame.midX, y: frame.midY)
+        circle.addGlow()
+        addChild(circle)
+        
+        // Animação breathing:
+        let scaleUp = SKAction.scale(to: 1.0, duration: 3.5)
+        let scaleDown = SKAction.scale(to: 0.5, duration: 3.5)
+        let sequence = SKAction.sequence([scaleDown, scaleUp])
+        let repeatForever = SKAction.repeatForever(sequence)
+        
+        circle.run(repeatForever)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     let circleSliderScene = SKScene(fileNamed: "CircleSliderScene")
+        self.view?.presentScene(circleSliderScene)
+    }
+}
+
---
+++ mini_challenge_1/TextCutscene/TextScene.swift
@@ -0,0 +1,39 @@
+//
+//  TextScene.swift
+//  spritekitanimation
+//
+//  Created by Thayna Rodrigues on 13/07/23.
+//
+import SpriteKit
+import GameplayKit
+
+class TextScene: SKScene {
+    
+        private var label: SKLabelNode!
+        
+        override func didMove(to view: SKView) {
+
+            label = SKLabelNode(fontNamed: "Futura")
+            label.text = "Você é um grão flutuando no espaço" // Separar os textos em outro arquivo
+            label.fontColor = SKColor.white
+            label.position = CGPoint(x: frame.midX, y: frame.midY)
+            addChild(label)
+            fadeLabelIn()
+        }
+        
+        // Cria um efeito de fade in no texto depois de 1 segundo de tela preta
+        private func fadeLabelIn() {
+            label.alpha = 0
+            let waitAction = SKAction.wait(forDuration: 1)
+            let fadeInAction = SKAction.fadeIn(withDuration: 5.0)
+            let fadeSequence = SKAction.sequence([waitAction, fadeInAction])
+            label.run(fadeSequence)
+        }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     let circleGlowScene = SKScene(fileNamed: "CircleGlowScene")
+        self.view?.presentScene(circleGlowScene)
+    }
+}
+
