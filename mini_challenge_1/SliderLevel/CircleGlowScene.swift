294173a8698abeccfeb5840cf3c92a46a8f13629
Thayna <thna.rdg@gmail.com>
2023-07-15 20:20:58 -0300

    Adiciona o efeito de tremer a câmera

--- mini_challenge_1/SliderLevel/CircleGlowScene.swift
+++ mini_challenge_1/SliderLevel/CircleGlowScene.swift
@@ -38,28 +38,7 @@ class CircleGlowScene: SKScene {
         let sequence = SKAction.sequence([scaleDown, scaleUp])
         let repeatForever = SKAction.repeatForever(sequence)
         
-        shakeCamera(layer: circle, duration: 5)
         circle.run(repeatForever)
-    }
-    
-    // Animação de shake
-    func shakeCamera(layer:SKSpriteNode, duration:Float) {
-
-        let amplitudeX:Float = 10;
-        let amplitudeY:Float = 6;
-        let numberOfShakes = duration / 0.04;
-        var actionsArray:[SKAction] = [];
-        for _ in 1...Int(numberOfShakes) {
-            let moveX = Float(arc4random_uniform(UInt32(amplitudeX))) - amplitudeX / 2;
-            let moveY = Float(arc4random_uniform(UInt32(amplitudeY))) - amplitudeY / 2;
-            let shakeAction = SKAction.moveBy(x: CGFloat(moveX), y: CGFloat(moveY), duration: 0.02);
-            shakeAction.timingMode = SKActionTimingMode.easeOut;
-            actionsArray.append(shakeAction);
-            actionsArray.append(shakeAction.reversed());
-        }
-
-        let actionSeq = SKAction.sequence(actionsArray);
-        layer.run(actionSeq);
     }
 
     
@@ -68,4 +47,3 @@ class CircleGlowScene: SKScene {
         self.view?.presentScene(circleSliderScene)
     }
 }
-
