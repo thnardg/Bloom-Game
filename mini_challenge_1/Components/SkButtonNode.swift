b356d4ee701bb434e4748f164509b6a6648c9857
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-07-17 13:57:05 -0300

    .

---
+++ mini_challenge_1/Components/SkButtonNode.swift
@@ -0,0 +1,32 @@
+import SpriteKit
+import Foundation
+
+class SkButtonNode:  SKNode {
+    var image:SKSpriteNode?
+    var label:SKLabelNode?
+    
+    
+    init(image:SKSpriteNode?, label:SKLabelNode?) {
+        self.image = image
+        self.label = label
+        
+        super.init()
+//        self.isUserInteractionEnabled = true
+        
+        if let image = image {
+            addChild(image)
+        }
+                
+        if let label = label {
+            label.fontSize = 25
+            label.position = CGPoint(x: 0, y: -10)
+            image?.addChild(label)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+}
+
