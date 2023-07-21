5ba30b0d489b80ab17c9ab222af22d6ab3c20f5d
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2023-07-21 17:23:29 -0300

    removendo botoes que nao serao usados

--- mini_challenge_1/Level01/BotoesMovimentacao.swift
+++ mini_challenge_1/Level01/BotoesMovimentacao.swift
@@ -1,25 +1,21 @@
 //
-//  Teste.swift
+//  BotoesMovimentacao.swift
 //  mini_challenge_1
 //
-//  Created by Thayna Rodrigues on 14/07/23.
+//  Created by Jairo Júnior on 21/07/23.
 //
 
 import Foundation
 import SpriteKit
 
-/*
-class Hud:GameScene{
+
+class BotoesMovimentacao:GameScene{
     var moveLeftButton: SkButtonNode!
     var moveRightButton: SkButtonNode!
     var isMovingLeft = false
     var isMovingRight = false
     
-    //var level01scene = Level01Scene()
-    var level01scene = Level01Scene()
-    var hud = Hud()
-    
-    func controles(){
+    func criar(){
         moveLeftButton = SkButtonNode(image: .init(color: .blue, size: CGSize(width: 50, height: 50)), label: .init(text: "Left"))
         
         if let button = moveLeftButton{
@@ -30,20 +26,12 @@ class Hud:GameScene{
         moveRightButton = SkButtonNode(image: .init(color: .blue, size: CGSize(width: 50, height: 50)), label: .init(text: "Right"))
         if let button = moveRightButton{
             addChild(button) // adding it to the scene's node tree
-            
         }
     }
     
-    override func didMove(to view: SKView) {
-        
-        level01scene.hud = hud
-        hud.level01scene = level01scene
-    }
-    
+   
     override func update(_ currentTime: TimeInterval) {
-        
         let characterSpeed: CGFloat = 5.0
-        
         if isMovingLeft {
             player.position.x -= characterSpeed
         } else if isMovingRight {
@@ -54,6 +42,7 @@ class Hud:GameScene{
         guard let touch = touches.first else { return }
         let touchLocation = touch.location(in: self)
         
+        //moving player buttons being activated to move/jump/return to the first scene
         if moveLeftButton.contains(touchLocation) {
             isMovingLeft = true
             player.xScale = -1
@@ -62,14 +51,12 @@ class Hud:GameScene{
             isMovingRight = true
             player.xScale = 1
             
-        }
+        } 
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
-        
         let touchLocation = touch.location(in: self)
         
-        //returning to false when u touch out the button and the player will stop to run
         if moveLeftButton.contains(touchLocation) {
             isMovingLeft = false
             
@@ -78,7 +65,6 @@ class Hud:GameScene{
             
         }
     }
+    
+    
 }
-
-
-*/
--- mini_challenge_1/Level01/Level01Scene.swift
+++ mini_challenge_1/Level01/Level01Scene.swift
@@ -15,10 +15,7 @@ class Level01Scene: GameScene, SKPhysicsContactDelegate { // first platformer le
    
     var jumpButton: SkButtonNode!
     var returnButton: SkButtonNode!
-    var moveLeftButton: SkButtonNode!
-    var moveRightButton: SkButtonNode!
-    var isMovingLeft = false
-    var isMovingRight = false
+    
     
    
     
@@ -43,8 +40,8 @@ class Level01Scene: GameScene, SKPhysicsContactDelegate { // first platformer le
     override func didMove(to view: SKView) { // loaded when reaching the level
         createButtons()
         //to hide the joystick
-        joystickBack.isHidden = true
-        joystickButton.isHidden = true
+        joystickBack.isHidden = false
+        joystickButton.isHidden = false
         jumpButton?.isHidden = true
         
         //adding them to the view
@@ -120,19 +117,6 @@ class Level01Scene: GameScene, SKPhysicsContactDelegate { // first platformer le
         
         if let button = jumpButton{
             addChild(button) // adding it to the scene's node tree
-            
-        }
-        moveLeftButton = SkButtonNode(image: .init(color: .blue, size: CGSize(width: 50, height: 50)), label: .init(text: "Left"))
-        
-        if let button = moveLeftButton{
-            addChild(button) // adding it to the scene's node tree
-            
-        }
-        
-        moveRightButton = SkButtonNode(image: .init(color: .blue, size: CGSize(width: 50, height: 50)), label: .init(text: "Right"))
-        if let button = moveRightButton{
-            addChild(button) // adding it to the scene's node tree
-            
         }
         
     }
@@ -167,27 +151,12 @@ class Level01Scene: GameScene, SKPhysicsContactDelegate { // first platformer le
         if velocityX > 0 {
             player.xScale = 1
         }
-        ///
-        ///
-        ///
-        //adding the speed that the player will run/walk
-        let characterSpeed: CGFloat = 5.0
-        
-        if isMovingLeft {
-            player.position.x -= characterSpeed
-        } else if isMovingRight {
-            player.position.x += characterSpeed
-        }
-        
+       
 
         if let camera = cameraNode{ // safe unwrapping the camera node
             camera.run(.group([.moveTo(x: player.position.x, duration: 0.25), .moveTo(y: player.position.y, duration: 0)]))
             
-            // fixing buttons to the camera
-            moveLeftButton?.position.x = camera.position.x - 300
-            moveLeftButton?.position.y = player.position.y - 100
-            moveRightButton?.position.x = camera.position.x - 200
-            moveRightButton?.position.y = player.position.y - 100
+            
             jumpButton?.position.x = camera.position.x  + 280
             jumpButton?.position.y = player.position.y - 50
             returnButton?.position.x = camera.position.x - 0
@@ -221,8 +190,7 @@ class Level01Scene: GameScene, SKPhysicsContactDelegate { // first platformer le
             }
         }
         // safe unwrapping button nodes
-        guard let moveLeftButton = moveLeftButton else { return }
-        guard let moveRightButton = moveRightButton else { return }
+        
         guard let jumpButton = jumpButton else { return }
         guard let returnButton = returnButton else { return }
         //see the first touche
@@ -230,16 +198,7 @@ class Level01Scene: GameScene, SKPhysicsContactDelegate { // first platformer le
         
         let touchLocation = touch.location(in: self)
         
-        //moving player buttons being activated to move/jump/return to the first scene
-        if moveLeftButton.contains(touchLocation) {
-            isMovingLeft = true
-            player.xScale = -1
-            
-        } else if moveRightButton.contains(touchLocation) {
-            isMovingRight = true
-            player.xScale = 1
-            
-        } else if jumpButton.contains(touchLocation) {
+        if jumpButton.contains(touchLocation) {
             jumpCharacter()
         }
         
@@ -262,13 +221,7 @@ class Level01Scene: GameScene, SKPhysicsContactDelegate { // first platformer le
         let touchLocation = touch.location(in: self)
         
         //returning to false when u touch out the button and the player will stop to run
-        if moveLeftButton.contains(touchLocation) {
-            isMovingLeft = false
-            
-        } else if moveRightButton.contains(touchLocation) {
-            isMovingRight = false
-            
-        }
+        
     }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches {
