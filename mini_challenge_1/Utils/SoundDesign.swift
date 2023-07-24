64c7d2fc700bdf52b20e652f74e4b717d78750f1
Thayna <thna.rdg@gmail.com>
2023-07-24 15:15:12 -0300

    Adicionando sound design

--- mini_challenge_1/Utils/SoundDesign.swift
+++ mini_challenge_1/Utils/SoundDesign.swift
@@ -1,8 +1,92 @@
-//
-//  SoundDesign.swift
-//  mini_challenge_1
-//
-//  Created by Thayna Rodrigues on 24/07/23.
-//
+import AVFoundation
 
-import Foundation
+class SoundDesign {
+    
+    static let shared = SoundDesign()
+    
+    private var backgroundMusicPlayer: AVAudioPlayer? // Reprodutor de música de fundo
+    private var soundEffectPlayer: AVAudioPlayer? // Reprodutor de efeito sonoro
+    
+    private init() {}
+    
+    // Função para adicionar uma música de fundo:
+    func playBackgroundMusic(filename: String) {
+        guard let music = Bundle.main.url(forResource: filename, withExtension: nil) else {
+            print("Arquivo da música de fundo não encontrado.")
+            return
+        }
+        
+        do {
+            backgroundMusicPlayer = try AVAudioPlayer(contentsOf: music) // Inicializa o reprodutor com o URL do arquivo de música
+            backgroundMusicPlayer?.numberOfLoops = -1 // Coloca a música em loop infinito
+            backgroundMusicPlayer?.volume = 0.5 // Volume em 50%
+            backgroundMusicPlayer?.play() // Inicia a música
+            
+        } catch {
+            print("Erro ao reproduzir a música de fundo: \(error.localizedDescription)")
+        }
+    }
+    
+    // Função para adicionar efeitos sonoros:
+    func playSoundEffect(filename: String) {
+        guard let sound = Bundle.main.url(forResource: filename, withExtension: nil) else {
+            print("Arquivo de efeito sonoro não encontrado.")
+            return
+        }
+        
+        do {
+            soundEffectPlayer = try AVAudioPlayer(contentsOf: sound)
+            soundEffectPlayer?.numberOfLoops = 0
+            soundEffectPlayer?.volume = 0.5 // Volume em 50%
+            soundEffectPlayer?.prepareToPlay() // Deixa o som preparado para tocar. Diminui possíveis delays
+            soundEffectPlayer?.play()
+        } catch {
+            print("Erro ao reproduzir o efeito sonoro: \(error.localizedDescription)")
+        }
+    }
+        
+    // Efeito de fade in da música:
+    func fadeInMusic(filename: String, duration: TimeInterval) {
+        guard let music = Bundle.main.url(forResource: filename, withExtension: nil) else {
+            print("Arquivo da música de fundo (fade-in) não encontrado.")
+            return
+        }
+        
+        do {
+            backgroundMusicPlayer = try AVAudioPlayer(contentsOf: music)
+            backgroundMusicPlayer?.numberOfLoops = -1
+            backgroundMusicPlayer?.volume = 0
+            backgroundMusicPlayer?.play()
+            backgroundMusicPlayer?.setVolume(0.5, fadeDuration: duration) // Determina o volume do fade-in e a duração
+            
+        } catch {
+            print("Erro ao reproduzir a música de fundo: \(error.localizedDescription)")
+        }
+    }
+    
+    // Efeito de fade-out da música (recebe um parâmetro opcional para checar se o efeito foi concluído):
+    func fadeOutMusic(duration: TimeInterval, completion: (() -> Void)?) {
+        guard let player = backgroundMusicPlayer else { return }
+        player.setVolume(0, fadeDuration: duration)
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) { // Obriga a próxima ação esperar a conclusão do fade-out.
+            completion?()
+        }
+    }
+    
+    // Efeito de fade-out do efeito sonoro (recebe um parâmetro opcional para checar se o efeito foi concluído):
+    func fadeOutSoundEffect(duration: TimeInterval, completion: (() -> Void)?) {
+        guard let player = soundEffectPlayer else { return }
+        player.setVolume(0, fadeDuration: duration)
+        DispatchQueue.main.asyncAfter(deadline: .now() + duration) { // Obriga a próxima ação esperar a conclusão do fade-out.
+            completion?()
+        }
+    }
+    
+    func stopSoundEffect() {
+        soundEffectPlayer?.stop()
+    }
+    
+    func stopBackgroundMusic() {
+        backgroundMusicPlayer?.stop()
+    }
+}
--- mini_challenge_1/Utils/VisualEffects.swift
+++ mini_challenge_1/Utils/VisualEffects.swift
@@ -1,15 +1,9 @@
-//
-//  VisualEffects.swift
-//  mini_challenge_1
-//
-//  Created by Thayna Rodrigues on 14/07/23.
-//
 
 import SpriteKit
 
 // Cria um efeito de brilho para ser aplicado em qualquer SKSpriteNode.
 extension SKSpriteNode {
-    func addGlow(radius: Float = 30) {
+    func addGlow(radius: Float) {
         let effectNode = SKEffectNode()
         effectNode.shouldRasterize = true
         addChild(effectNode)
@@ -20,4 +14,12 @@ extension SKSpriteNode {
         effectNode.addChild(effect)
         effectNode.filter = CIFilter(name: "CIGaussianBlur", parameters: ["inputRadius":radius])
     }
+    
+    func removeGlow() {
+            for child in children {
+                if let effectNode = child as? SKEffectNode {
+                    effectNode.removeFromParent()
+                }
+            }
+        }
 }
